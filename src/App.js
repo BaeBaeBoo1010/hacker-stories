8516a38d8694a09b5eb7c0cc67f5a2ed34a33f0b
Le Xuan Khanh <khanhle14062006@gmail.com>
2025-06-10 22:09:15 +0700

    minor changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ứng dụng đếm</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
